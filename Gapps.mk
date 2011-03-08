27516294869bc4a3aa1e4865f0b30599bfbb7bad
mnlk2-custom <mottier.ludovic@free.fr>
2011-03-08 22:32:45 +0100

    update config update gapps add overlay

--- Gapps.mk
+++ Gapps.mk
@@ -1,12 +1,8 @@
-# GSM APN list
-PRODUCT_COPY_FILES += \
-    	vendor/htc/passion/gapps/apns-conf.xml:./system/etc/apns-conf.xml
 
 #copy Gapps
 ifdef WITH_GOOGLE
     PRODUCT_COPY_FILES += \
-	vendor/htc/passion/gapps/BooksPhone.apk:./system/app/BooksPhone.apk \
-	vendor/htc/passion/gapps/CarHomeGoogle.apk:./system/app/CarHomeLauncher.apk \
+	vendor/htc/passion/gapps/CarHomeLauncher.apk:./system/app/CarHomeLauncher.apk \
 	vendor/htc/passion/gapps/Facebook.apk:./system/app/Facebook.apk \
 	vendor/htc/passion/gapps/FOTAKill.apk:./system/app/FOTAKill.apk \
 	vendor/htc/passion/gapps/GenieWidget.apk:./system/app/GenieWidget.apk \
@@ -18,9 +14,9 @@ ifdef WITH_GOOGLE
 	vendor/htc/passion/gapps/GooglePartnerSetup.apk:./system/app/GooglePartnerSetup.apk \
 	vendor/htc/passion/gapps/GoogleQuickSearchBox.apk:./system/app/GoogleQuickSearchBox.apk \
 	vendor/htc/passion/gapps/GoogleServicesFramework.apk:./system/app/GoogleServicesFramework.apk \
-	vendor/htc/passion/gapps/HtcCopyright.apk:./system/app/HtcCopyright.apk \
-	vendor/htc/passion/gapps/HtcEmailPolicy.apk:./system/app/HtcEmailPolicy.apk \
 	vendor/htc/passion/gapps/HtcSettings.apk:./system/app/HtcSettings.apk \
+	vendor/htc/passion/gapps/HtcEmailPolicy.apk:./system/app/HtcEmailPolicy.apk \
+	vendor/htc/passion/gapps/HtcCopyright.apk:./system/app/HtcCopyright.apk \
 	vendor/htc/passion/gapps/LatinImeTutorial.apk:./system/app/LatinImeTutorial.apk \
 	vendor/htc/passion/gapps/LiveWallpapers.apk:./system/app/LiveWallpapers.apk \
 	vendor/htc/passion/gapps/MarketUpdater.apk:./system/app/MarketUpdater.apk \
@@ -30,15 +26,14 @@ ifdef WITH_GOOGLE
 	vendor/htc/passion/gapps/PassionQuickOffice.apk:./system/app/PassionQuickOffice.apk \
 	vendor/htc/passion/gapps/SetupWizard.apk:./system/app/SetupWizard.apk \
 	vendor/htc/passion/gapps/Talk.apk:./system/app/Talk.apk \
-	vendor/htc/passion/gapps/Twitter.apk:./system/app/Twitter.apk \
 	vendor/htc/passion/gapps/Vending.apk:./system/app/Vending.apk \
 	vendor/htc/passion/gapps/com.google.android.maps.jar:./system/framework/com.google.android.maps.jar \
 	vendor/htc/passion/gapps/com.google.android.maps.xml:./system/etc/permissions/com.google.android.maps.xml \
 	vendor/htc/passion/gapps/features.xml:./system/etc/permissions/features.xml \
-	vendor/htc/passion/gapps/kickback.xml:./system/etc/permissions/kickback.xml \
 	vendor/htc/passion/gapps/libvoicesearch.so:./system/lib/libvoicesearch.so \
-	vendor/htc/passion/gapps/soundback.apk:./system/lib/soundback.apk \
-	vendor/htc/passion/gapps/talkback.apk:./system/lib/talkback.apk \
 	vendor/htc/passion/gapps/google_generic_update.txt:./system/etc/updatecmds/google_generic_update.txt
-endif
+else
+    PRODUCT_PACKAGES += \
+        Provision
 
+endif
